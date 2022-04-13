f5526a4921cc44402b5cf838c1cb9fca5b51b191
boseong94 <boseong.kim@catenoid.net>
2022-04-13 21:49:06 +0900

    color control

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
 
 canvas.width = 500;
 canvas.height = 500;
 
 ctx.strokeStyle = '#2c2c2c';
-ctx.lineWidth = 1;
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
 
 function startPainting () {
     painting = true;
@@ -35,3 +41,7 @@ if (canvas) {
    canvas.addEventListener('mouseup', stopPainting);
    canvas.addEventListener('mouseleave', stopPainting);
 }
+
+Array.from(colors).forEach(color =>
+    color.addEventListener("click", handleColorClick)
+);
